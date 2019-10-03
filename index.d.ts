c1686fe4185ba0c5a4e96e928f237c60668a7f48
Shukhrat <shukhrat@ismax.ru>
2019-10-04 01:48:43 +0300

    Extend type definition

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
+import {Response} from "express-serve-static-core";
+
+declare module "express-serve-static-core" {
+    export interface Response {
+        promiseRender: (view: string, options?: object) => Promise<string>;
+    }
+}
 declare function promiseRender(): void;
 
-export = promiseRender;
+export default promiseRender;
